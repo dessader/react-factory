713f5743d7e482083f8cf7ed56f4120540df5f64
dessader <dessader.work@gmail.com>
2026-07-18 22:06:48 +0300

    Update prettier config

--- prettier.config.ts
+++ prettier.config.ts
@@ -1,7 +1,15 @@
-import type { Config } from "prettier";
+import type { Config } from 'prettier';
 
 const config: Config = {
   singleQuote: false,
+  semi: true,
+  jsxSingleQuote: true,
+  trailingComma: 'all',
+  printWidth: 80,
+  tabWidth: 2,
+  endOfLine: 'auto',
+  bracketSpacing: true,
+  bracketSameLine: false,
 };
 
 export default config;
