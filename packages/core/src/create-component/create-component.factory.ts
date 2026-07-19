8550fd8c67f969ec28eb269247fd776bb6075b9d
dessader <dessader.work@gmail.com>
2026-07-19 12:31:30 +0300

    Move create-component factory from draft

---
+++ packages/core/src/create-component/create-component.factory.ts
@@ -0,0 +1,85 @@
+import { memo as reactMemo, cloneElement, isValidElement } from "react";
+import { getComponentDisplayName } from "../utils";
+
+import type {
+  CreateComponentFactoryOptions,
+  CreateComponentFactoryComponentProps,
+  CreateComponentFactoryResolvedComponentProps,
+} from "./create-component.types";
+import type { ElementType, ReactNode } from "react";
+
+export const createComponent = <
+  TCustomProps extends Record<string, unknown> = Record<never, never>,
+>() => {
+  return <
+    TElement extends ElementType = "div",
+    TPolymorphic extends boolean = true,
+  >({
+    element,
+    name,
+    memo,
+    polymorphic,
+    Render,
+  }: CreateComponentFactoryOptions<TElement, TCustomProps> & {
+    polymorphic?: TPolymorphic;
+  }) => {
+    const resolvedElement = element ?? ("div" as TElement);
+    const resolvedName = name ?? getComponentDisplayName(resolvedElement);
+
+    const FactoryComponent = <TAs extends ElementType = TElement>(
+      props: CreateComponentFactoryComponentProps<
+        TAs,
+        TCustomProps,
+        TPolymorphic
+      >,
+    ): ReactNode => {
+      let resolvedComponent: ElementType;
+      let cleanedProps: object;
+
+      if (polymorphic) {
+        const { component, ...rest } = props as { component?: TAs } & object;
+        resolvedComponent = component ?? resolvedElement;
+        cleanedProps = rest;
+      } else {
+        resolvedComponent = resolvedElement;
+        cleanedProps = props;
+      }
+
+      const result = Render(
+        resolvedComponent as TElement,
+        cleanedProps as unknown as CreateComponentFactoryResolvedComponentProps<
+          TElement,
+          TCustomProps
+        >,
+      );
+
+      if (result instanceof Promise) {
+        return result.then((resolved) =>
+          isValidElement(resolved)
+            ? cloneElement(resolved, {
+                "data-component": resolvedName,
+              } as never)
+            : resolved,
+        );
+      }
+
+      if (isValidElement(result)) {
+        return cloneElement(result, {
+          "data-component": resolvedName,
+        } as never);
+      }
+
+      return result;
+    };
+
+    FactoryComponent.displayName = resolvedName;
+
+    if (memo) {
+      return reactMemo(
+        FactoryComponent,
+        typeof memo === "function" ? memo : undefined,
+      ) as typeof FactoryComponent;
+    }
+    return FactoryComponent;
+  };
+};
